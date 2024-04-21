8d88bc44bed864718b55a2520cbad880a6347e1a
Serpent <167712387+ProgrammingSerpent@users.noreply.github.com>
2024-04-22 01:08:39 +0500

    Create practice_dynamic.py

---
+++ modules/practice_dynamic.py
@@ -0,0 +1,15 @@
+name = "Alex"
+
+print("Имя: " + name)
+
+age = 18
+
+print("Возраст: " + str(age))
+
+age = age + 1
+
+print("Новый возраст: " + str(age))
+
+is_student = True
+
+print("Студент: " + str(is_student))
