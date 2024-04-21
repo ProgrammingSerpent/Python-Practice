c8db8dc5a6d82d36c661a0f1f8fb2fe7c5086d2d
Serpent <167712387+ProgrammingSerpent@users.noreply.github.com>
2024-04-22 01:24:02 +0500

    Create practice_organization.py

---
+++ modules/practice_organization.py
@@ -0,0 +1,13 @@
+my_string = input()
+
+print("Длина: " + str(len(my_string)))
+
+print("Верхний регистр: " + my_string.upper())
+
+print("Нижний регистр: " + my_string.lower())
+
+print("Уничтожение пробелов: " + my_string.replace(" ", ""))
+
+print("Первый символ: " + str(my_string[0]))
+
+print("Последний символ: " + str(my_string[-1]))
