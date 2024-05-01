8eae1caa7fd7170d0c72100b845ee2f09af5d76d
Serpent <167712387+ProgrammingSerpent@users.noreply.github.com>
2024-05-01 16:54:18 +0500

    Dictionaries

---
+++ modules/practice_dict.py
@@ -0,0 +1,25 @@
+print("Список:")
+my_list = ["apple", "banana", "orange", "lemon", "kiwi"]
+
+print(my_list)
+
+print(my_list[0])
+
+print(my_list[-1])
+
+print(my_list[3:5])
+
+my_list[3] = "grape"
+
+print(my_list)
+
+my_dict = {"слон": "elephant", "сыр": "cheese", "молоко": "milk"}
+
+print("\nСловарь:")
+print(my_dict)
+
+print(my_dict["слон"])
+
+my_dict.setdefault("кот", "cat")
+
+print(my_dict)
